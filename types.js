635ecf2bee028e6bb5ccc2a999c169904fa3c693
Edrian Hernandez <117151322+EdrianHernandez@users.noreply.github.com>
2026-01-19 23:20:06 +0800

    Update and rename types.ts to types.js

---
+++ types.js
@@ -0,0 +1,17 @@
+/**
+ * Vehicle types supported by the transit system
+ */
+export const VehicleType = {
+  BUS: 'BUS',
+  TRAIN: 'TRAIN',
+  TRAM: 'TRAM'
+};
+
+/**
+ * Real-time status indicators for vehicles and routes
+ */
+export const Status = {
+  ON_TIME: 'ON_TIME',
+  DELAYED: 'DELAYED',
+  OUT_OF_SERVICE: 'OUT_OF_SERVICE'
+};
